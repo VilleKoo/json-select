c71297585830502f537391c24cd7002f32149184
VilleKoo <localzero@gmail.com>
2015-09-17 16:42:21 +0300

    Update json-select.js

--- js/json-select.js
+++ js/json-select.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
+    $("#load").show();
     $.getJSON( 
-    "data.json", 
-    function( jsonData ) {
+    "data.json",
+    $("#load").fadeOut(300), 
+    function(jsonData) {
       	
     var lang = $('html').attr('lang'),
         url  = lang  + "-url",
@@ -12,14 +14,20 @@ $(document).ready(function () {
         for (var i = 0; i < jsonData.Table.length; i++) {
           listItems += "<option data-url='" + jsonData.Table[i][url] + "' value='" + jsonData.Table[i].Code + "'>" + jsonData.Table[i][name] + "</option>";
     	}
-        
-        $('#cancer').html(listItems).change(function(){
-        	var datalink = $('select option:selected').attr('data-url');
 
-            $('#continue').prop('disabled', false).text('continue').click(function(){
-                window.location = datalink;
-            });
-            
+        $('#list').html(listItems).change(function(){
+        	var datalink = $('#list option:selected').attr('data-url');
+            var code = $('#list').val();
+
+            if ( code < 1 ) {
+                $('#continue').prop('disabled', true).html('x');
+            }
+
+            else {
+                $('#continue').prop('disabled', false).html('jatka').click(function(){
+                    window.location = datalink;
+                });
+            }     
         });
     });
 });
